8ac00cccc970f2ff4842439381fb3f954e5f19c0
Knotonic <knotonicdevdesign@gmail.com>
2022-10-08 17:10:34 +0530

    testing2

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express=require('express')
 
 const app=express();
  app.get("/",(req,res)=>{
-    res.send({"testing":"1"})
+    res.send({"testing":"2"})
  })
 
 
 app.listen(4000,()=>{
-console.log("testing the cicd pipeline");
+console.log("testing the cicd pipeline versiont2");
 })
