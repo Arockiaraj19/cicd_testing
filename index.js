c8d6dd0756a56482f777cad45af7f044903d81d0
Knotonic <knotonicdevdesign@gmail.com>
2022-10-08 17:04:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+
+const app=express();
+ app.get("/",(req,res)=>{
+    res.send({"testing":"1"})
+ })
+
+
+app.listen(4000,()=>{
+console.log("testing the cicd pipeline");
+})
